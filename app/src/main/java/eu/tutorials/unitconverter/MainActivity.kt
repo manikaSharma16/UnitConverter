a4339c36badb3a8a57851029af324d4899ac58a7
Manika Sharma <manika.sharma10000@gmail.com>
2025-03-25 16:11:22 +0100

    Button and Toast

--- app/src/main/java/eu/tutorials/unitconverter/MainActivity.kt
+++ app/src/main/java/eu/tutorials/unitconverter/MainActivity.kt
@@ -1,17 +1,20 @@
 package eu.tutorials.unitconverter
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import eu.tutorials.unitconverter.ui.theme.UnitConverterTheme
 
@@ -40,7 +43,12 @@ fun UnitConverter(){
         OutlinedTextField(value = "Enter Value", onValueChange = { // Here goes what should happen when the value of our OutlinedTextField changes
              })
         Row {
-
+            val context = LocalContext.current
+            Button(onClick = { Toast.makeText(context,
+                "Thanks for clicking!", Toast.LENGTH_LONG).show()
+            }) {
+                Text(text = "Click Me!")
+            }
         }
         Text(text = "Result:")
     }
